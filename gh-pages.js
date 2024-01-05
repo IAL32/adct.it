ac364aa606c9dce034ac8101b40b1a15a09b53d6
IAL32 <adrian.castro@tacto.ai>
2024-01-05 09:44:54 +0100

    Update email

--- gh-pages.js
+++ gh-pages.js
@@ -7,7 +7,7 @@ publish(
     repo: 'https://github.com/IAL32/adct.it.git', // Update to point to your repository
     user: {
       name: 'IAL32', // update to use your name
-      email: 'adrian.d.castro.t@gmail.com' // Update to use your email
+      email: '562969+IAL32@users.noreply.github.com' // Update to use your email
     },
     dotfiles: true
   },
